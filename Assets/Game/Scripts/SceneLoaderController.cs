b77ab9969bddac228c430592b35825fc81244a35
dkbozkurt <d.k.bozkurt@gmail.com>
2022-03-26 21:12:45 +0300

    Title ,Icon Changes

--- Assets/Game/Scripts/SceneLoaderController.cs
+++ Assets/Game/Scripts/SceneLoaderController.cs
@@ -62,6 +62,11 @@ namespace Game.Scripts
             {
                 SceneDecider(SceneName.Scene3.ToString());
             }
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
             
         }
         
